42d036c1e84c81041b3281bdbcc341785bc15950
SaraQian <abigail830@163.com>
2020-03-05 09:45:33 +0800

    init commit

---
+++ src/main/java/com/github/tuding/blindbox/BlindboxApplication.java
@@ -0,0 +1,13 @@
+package com.github.tuding.blindbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlindboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tuding/blindbox/BlindboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tuding.blindbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
