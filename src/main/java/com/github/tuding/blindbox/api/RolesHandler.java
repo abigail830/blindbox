17ddf9add9937ad10d023e09a5d214b426674d36
bryan-cheung <bryan.l.cheung@gmail.com>
2020-03-10 23:31:36 +0800

    add query and delete endpoint for roles

--- src/main/java/com/github/tuding/blindbox/api/RolesHandler.java
+++ src/main/java/com/github/tuding/blindbox/api/RolesHandler.java
@@ -1,20 +1,26 @@
 package com.github.tuding.blindbox.api;
 
 import com.github.tuding.blindbox.api.dto.RoleDTO;
+import com.github.tuding.blindbox.exception.RolesNotFoundException;
 import com.github.tuding.blindbox.infrastructure.repository.RolesRepository;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.websocket.server.PathParam;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -23,16 +29,21 @@ public class RolesHandler {
     @Autowired
     RolesRepository rolesRepository;
 
+    @Value("${app.imagePath}")
+    private String imagePath;
+
+
     @PostMapping("/{name}")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "请求成功")})
-    public void uploadRoles(@PathParam("name") String name,
+    public void uploadRoles(@PathVariable("name") String name,
                             @RequestParam("category") String category,
                             @RequestParam("description") String description,
                             @RequestParam("image")MultipartFile file) throws IOException {
 
         log.info("handle role creation as name {} category {} description {}", name, category, description);
         //TODO: image format/size check
-        File storeFile = new File("/" + name + " .png");
+
+        File storeFile = new File( getRolesFolder() + name + ".png");
         file.transferTo(storeFile);
 
         RoleDTO roleDTO = new RoleDTO();
@@ -42,4 +53,50 @@ public class RolesHandler {
         roleDTO.setRoleImage(storeFile.getCanonicalPath());
         rolesRepository.saveUserWithOpenId(roleDTO);
     }
+
+    @DeleteMapping("/{name}")
+    public void deleteRoles(@PathVariable("name") String name) {
+        rolesRepository.deleteRoles(name);
+    }
+
+    @GetMapping("/")
+    public List<RoleDTO> getRoles() {
+        return rolesRepository.queryRoles();
+    }
+
+    @GetMapping("/{name}")
+    public RoleDTO getRole(@PathVariable("name") String name) {
+        Optional<RoleDTO> roleDTOOptional = rolesRepository.queryRolesByName(name);
+        if (roleDTOOptional.isPresent()) {
+            return roleDTOOptional.get();
+        } else {
+            throw new RolesNotFoundException();
+        }
+    }
+
+
+    @GetMapping("/{name}/images")
+    public ResponseEntity<Resource> getRoleImage(@PathVariable("name") String name) throws FileNotFoundException {
+        Optional<RoleDTO> roleDTOOptional = rolesRepository.queryRolesByName(name);
+        if (roleDTOOptional.isPresent()) {
+            File file = new File(roleDTOOptional.get().getRoleImage());
+            InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+            return ResponseEntity.ok()
+                    .contentLength(file.length())
+                    .contentType(MediaType.parseMediaType("image/png"))
+                    .body(resource);
+        } else {
+            throw new RolesNotFoundException();
+        }
+
+    }
+
+    public String getRolesFolder() {
+        File rolesFolder = new File(imagePath + "/roles/");
+        if (!rolesFolder.exists()) {
+            rolesFolder.mkdir();
+        }
+        return imagePath + "/roles/";
+    }
+
 }
--- src/main/java/com/github/tuding/blindbox/infrastructure/repository/RolesRepository.java
+++ src/main/java/com/github/tuding/blindbox/infrastructure/repository/RolesRepository.java
@@ -4,8 +4,13 @@ import com.github.tuding.blindbox.api.dto.RoleDTO;
 import com.github.tuding.blindbox.infrastructure.util.Toggle;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Slf4j
@@ -14,17 +19,36 @@ public class RolesRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    private RowMapper<RoleDTO> rowMapper = new BeanPropertyRowMapper<>(RoleDTO.class);
+
+
     public void saveUserWithOpenId(RoleDTO roleDTO) {
-        log.info("Going to insert role_tbl with name: {}", roleDTO.getName());
+        log.info("Going to insert roles_tbl with name: {}", roleDTO.getName());
 
         if (Toggle.TEST_MODE.isON()) {
-            String insertSql = "INSERT INTO role_tbl (name, category, description, role_image) " +
+            String insertSql = "INSERT INTO roles_tbl (name, category, description, role_image) " +
                     "VALUES (?, ?, ?, ?)";
             jdbcTemplate.update(insertSql, roleDTO.getName(), roleDTO.getCategory(), roleDTO.getDescription(), roleDTO.getRoleImage());
         } else {
-            String insertSql = "INSERT ignore INTO role_tbl (name, category, description, role_image) " +
+            String insertSql = "INSERT ignore INTO roles_tbl (name, category, description, role_image) " +
                     "VALUES (?, ?, ?, ?)";
             jdbcTemplate.update(insertSql, roleDTO.getName(), roleDTO.getCategory(), roleDTO.getDescription(), roleDTO.getRoleImage());
         }
+    }
+
+    public Optional<RoleDTO> queryRolesByName(String name) {
+        log.info("Going to query roles with name: {}", name);
+        List<RoleDTO> roleDTOs = jdbcTemplate.query("SELECT * FROM roles_tbl WHERE name = ?", rowMapper, name);
+        return roleDTOs.stream().findFirst();
+    }
+
+    public List<RoleDTO> queryRoles() {
+        log.info("Going to query roles ");
+        return jdbcTemplate.query("SELECT * FROM roles_tbl", rowMapper);
+    }
+
+    public void deleteRoles(String name) {
+        log.info("Delete role for {}", name);
+        jdbcTemplate.update("DELETE FROM roles_tbl WHERE name = ?", name);
     }
 }
