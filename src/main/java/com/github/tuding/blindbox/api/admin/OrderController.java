2ac343df2ceeb4c392e7e28f6609785a4e924143
bryan-cheung <bryan.l.cheung@gmail.com>
2020-04-06 14:24:48 +0800

    Draft admin ui for displaying order

--- src/main/java/com/github/tuding/blindbox/api/admin/OrderController.java
+++ src/main/java/com/github/tuding/blindbox/api/admin/OrderController.java
@@ -1,21 +1,34 @@
 package com.github.tuding.blindbox.api.admin;
 
+import com.github.tuding.blindbox.domain.order.Order;
+import com.github.tuding.blindbox.domain.order.OrderStatus;
+import com.github.tuding.blindbox.infrastructure.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin-ui/orders")
 public class OrderController {
 
+    @Autowired
+    OrderRepository orderRepository;
+
     @GetMapping("/")
     public String greeting(Model model) {
-        //TODO: to query all order in brief format
+        List<Order> allOutstandingOrder =
+                orderRepository.getAllOrder();
 
-        model.addAttribute("orders", new ArrayList<>());
+        model.addAttribute("orders", allOutstandingOrder);
         return "order";
+    }
+
+    @PutMapping("/deliver/{orderId}")
+    public @ResponseBody
+    void placeOrder(@PathVariable String orderId) {
+        orderRepository.updateOrderStatus(orderId, OrderStatus.DELIVERED.name());
     }
 }
