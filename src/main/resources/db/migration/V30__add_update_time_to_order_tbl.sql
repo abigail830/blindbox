bf74486a921dde6b6b43a24d38e3b215fb226419
SaraQian <abigail830@163.com>
2020-10-22 14:52:17 +0800

    fix: adding data patch for existing NEW_TRANSPORT entries

--- src/main/resources/db/migration/V30__add_update_time_to_order_tbl.sql
+++ src/main/resources/db/migration/V30__add_update_time_to_order_tbl.sql
@@ -1,2 +1,4 @@
 alter table order_tbl
 ADD updatedTime timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间';
+
+UPDATE order_tbl set updatedTime=now() where status = 'NEW_TRANSPORT';
